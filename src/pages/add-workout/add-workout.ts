5ab269cf3693ad506ea4e50ca31860a89ab5498b
Branislav <remen124@gmail.com>
2017-05-11 21:49:32 +0200

    update readme

--- src/pages/add-workout/add-workout.ts
+++ src/pages/add-workout/add-workout.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { WorkoutService } from '../../app/services/workout.service';
+import { WorkoutsPage } from '../workouts/workouts';
 
 @Component({
   selector: 'add-workout',
@@ -7,8 +9,30 @@ import { NavController } from 'ionic-angular';
 })
 export class AddWorkoutPage {
 
-  constructor(public navCtrl: NavController) {
+  public title: String;
+  public note: String;
+  public type: String;
+  public result: any;
 
+  constructor(public navCtrl: NavController, private workoutService: WorkoutService) {
   }
+
+  onSubmit(){
+    var workout = {
+      title: this.title,
+      note: this.note,
+      type: this.type,
+    }
+
+    //Add workout
+    this.workoutService.addWorkout(workout)
+      .subscribe(data => {
+        this.result = data;
+      });
+
+    this.navCtrl.push(WorkoutsPage);
+  }
+
+
 
 }
